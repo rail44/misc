f464e03e93a19ab82e2ae22db2195177955c5738
Satoshi Amemiya <amemiya@protonmail.com>
2015-06-21 23:06:47 +0900

    Update config.fish to use palette color scheme

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-eval sh $HOME/.config/fish/base16-shell/base16-solarized.light.sh
+eval sh $HOME/.config/fish/base16-shell/base16-palette.light.sh
 
 set -x GOPATH $HOME/go
 set -x PATH $HOME/go/bin $PATH
